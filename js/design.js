93fbf25ce615d0ccda40679ade115affe3a39401
Brody <brodywilton@hotmail.com>
2018-06-15 14:20:14 +0100

    Add variables for table width, height and user color choice

---
+++ js/design.js
@@ -0,0 +1,14 @@
+// Select color input
+let colorInput = document.getElementById('colorPicker').value;
+
+// Select size input
+let gridHeight = document.getElementById('inputHeight').value;
+let gridWidth = document.getElementById('inputWidth').value;
+
+
+// When size is submitted by the user, call makeGrid()
+
+
+function makeGrid() {
+
+}
