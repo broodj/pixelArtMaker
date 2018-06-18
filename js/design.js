6a77353aa89af469f258f7e894378ff412a7e4a2
Brody <brodywilton@hotmail.com>
2018-06-18 12:35:45 +0100

    Change variables to use querySelector instead of getElementById

--- js/design.js
+++ js/design.js
@@ -1,9 +1,20 @@
 // Select color input
-let colorInput = document.getElementById('colorPicker').value;
+let colorInput = document.querySelector('#colorPicker').value;
 
 // Select size input
-let gridHeight = document.getElementById('inputHeight').value;
-let gridWidth = document.getElementById('inputWidth').value;
+let gridHeight = document.querySelector('#inputHeight').value;
+let gridWidth = document.querySelector('#inputWidth').value;
+
+const table = document.querySelector('#pixelCanvas');
+
+
+
+
+
+
+
+
+
 
 
 // When size is submitted by the user, call makeGrid()
