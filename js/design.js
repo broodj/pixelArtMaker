edae8fcfa052f892798386c9266473f00e258f39
Brody <brodywilton@hotmail.com>
2018-06-26 11:24:28 +0100

    Add makeGrid() function

--- js/design.js
+++ js/design.js
@@ -2,12 +2,25 @@
 let submit = document.querySelector('input[type="submit"]');
 let canvas = document.querySelector('#pixelCanvas');
 
-let rows = document.querySelector('#inputHeight').value;
-let columns = document.querySelector('inputWidth').value;
-
 let color = document.querySelector('#colorPicker').value;
 
 //add makeGrid();
+function makeGrid(){
+	let rows = document.querySelector('#inputHeight').value;
+	let columns = document.querySelector('inputWidth').value;
+	canvas.innerHTML = "";
+
+	for (let i = 0; i < rows; i++){
+		let row = document.createElement('TR');
+		canvas.appendChild(row);
+
+		for (let j = 0; j < rows; j++){
+			let col = document.createElement('TD');
+			row.appendChild(col);
+		}
+	}
+}
+
 
 //add event listeners
 
