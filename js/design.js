4dd5e1baf022b775a8dd7b031fd1706b4f20bda5
Brody <brodywilton@hotmail.com>
2018-06-26 11:21:17 +0100

    Add variables to store interactive page elements and dimensions of grid

--- js/design.js
+++ js/design.js
@@ -1,4 +1,11 @@
 //add variables to interact with
+let submit = document.querySelector('input[type="submit"]');
+let canvas = document.querySelector('#pixelCanvas');
+
+let rows = document.querySelector('#inputHeight').value;
+let columns = document.querySelector('inputWidth').value;
+
+let color = document.querySelector('#colorPicker').value;
 
 //add makeGrid();
 
